163b888b3663ce0241ae09aefb01667379290deb
SarvajeetPatel <sarvajeet@zealousweb.com>
2024-04-15 19:34:47 +0530

    implemented name functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,40 @@ import { Typography, Button, Box, useTheme, MobileStepper } from '@mui/material'
 import QuizQues from './components/QuizQues'
 import { KeyboardArrowRight, KeyboardArrowLeft } from '@mui/icons-material'
 import Result from './components/Result'
+import { useFormik } from 'formik'
 
-export default function App() {
-  const [activeStep, setActiveStep] = useState(0);
-  const [skipped, setSkipped] = useState(new Set<number>());
+type formikValues = {
+  name?: string,
+  step?: number,
+  answers?: [],
+  validUser?: boolean
+}
+
+export default function App(props: formikValues) {
   const theme = useTheme();
-  const [answers, setAnswers] = useState([]);
   const [timeLeft, setTimeLeft] = useState(QuizQues.length * 30);
   const [mins, setMins] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      name: props.name || '',
+      step: props.step || 0,
+      answers: props.answers || [],
+      validUser: props.validUser || false
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
   useEffect(() => {
     const storedData = localStorage.getItem('currentData');
-    const storedTime = localStorage.getItem('time');
     if (storedData) {
       const parsedData = JSON.parse(storedData);
-      setActiveStep(parsedData.step || 0);
-      setAnswers(parsedData.answers || []);
-    }
-    if (storedTime) {
-      setTimeLeft(parseInt(storedTime, 10));
+      values.step = parsedData.step || 0;
+      values.answers = parsedData.answers || [];
+      setTimeLeft(parseInt(parsedData.time, 10));
     }
   }, []);
 
@@ -49,174 +63,169 @@ export default function App() {
       setSeconds(timeLeft);
     }
     if (timeLeft === 0) {
-      setActiveStep(QuizQues.length);
+      values.step = QuizQues.length;
     }
-    localStorage.setItem('time', timeLeft.toString());
+    const tempData = {
+      name: values.name,
+      step: values.step,
+      time: timeLeft.toString(),
+      answers: values.answers
+    }
+    localStorage.setItem('currentData', JSON.stringify(tempData));
   }, [timeLeft]);
-
-  const isStepOptional = (step: number) => {
-    return step < QuizQues.length;
-  };
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
 
   const handleNext = () => {
     const tempData = {
-      'step': activeStep + 1,
-      'answers': answers
+      name: values.name,
+      step: values.step + 1,
+      time: timeLeft,
+      answers: values.answers
     }
     localStorage.setItem('currentData', JSON.stringify(tempData))
 
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values())
-      newSkipped.delete(activeStep)
-    }
-
-    if (activeStep < QuizQues.length - 1) {
-      setActiveStep((prevActiveStep: any) => prevActiveStep + 1)
-    } else if ((activeStep + 1) === QuizQues.length) {
-      setActiveStep((prevActiveStep: any) => prevActiveStep + 1)
+    if (values.step < QuizQues.length - 1) {
+      values.step = values.step + 1
+    } else if ((values.step + 1) === QuizQues.length) {
+      values.step = values.step + 1
       setMins(0)
       setSeconds(0)
       setTimeLeft(0)
     }
-    setSkipped(newSkipped)
   };
 
   const handleBack = () => {
     const tempData = {
-      'step': activeStep - 1,
-      'answers': answers
+      name: values.name,
+      step: values.step - 1,
+      time: timeLeft,
+      answers: values.answers
     }
     localStorage.setItem('currentData', JSON.stringify(tempData))
-    setActiveStep((prevActiveStep: any) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    const tempData = {
-      'step': activeStep + 1,
-      'answers': answers
-    }
-    localStorage.setItem('currentData', JSON.stringify(tempData))
-
-    setActiveStep((prevActiveStep: any) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
+    values.step = values.step - 1;
   };
 
   const handleReset = () => {
     localStorage.removeItem('currentData')
-    localStorage.removeItem('time')
-    setAnswers([])
-    setActiveStep(0)
+    values.name = ''
+    values.answers = []
+    values.step = 0
+    values.validUser = false
     setTimeLeft(QuizQues.length * 30)
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChoice = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setAnswers(prevAnswers => {
-      const updatedAnswers: any = [...prevAnswers];
-      const index = updatedAnswers.findIndex((ans: any) => ans.que === name);
-      if (index !== -1) {
-        updatedAnswers[index].ans = value;
-      } else {
-        updatedAnswers.push({ que: name, ans: value });
-      }
-      localStorage.setItem('currentData', JSON.stringify({ step: activeStep, answers: updatedAnswers }));
-      return updatedAnswers;
-    });
+    const updatedAnswers: any = [...values.answers];
+    const index = updatedAnswers.findIndex((ans: any) => ans.que === name);
+    if (index !== -1) {
+      updatedAnswers[index].ans = value;
+    } else {
+      updatedAnswers.push({ que: name, ans: value });
+    }
+    localStorage.setItem('currentData', JSON.stringify({ name: values.name, step: values.step, time: timeLeft, answers: updatedAnswers }));
+    values.answers = updatedAnswers;
   };
 
   const handleTabs = (i: number) => {
-    if (activeStep !== QuizQues.length) {
-      const userInput = {
-        'step': i,
-        'answers': answers
+    if (values.step !== QuizQues.length) {
+      const tempData = {
+        name: values.name,
+        step: values.step,
+        time: timeLeft,
+        answers: values.answers
       }
-      localStorage.setItem('current data', JSON.stringify(userInput))
-      setActiveStep(i)
+      localStorage.setItem('currentData', JSON.stringify(tempData))
+      values.step = i
     } else {
       alert('Test ended! You cannot go back, but can restart it!')
     }
   }
 
+  const handlePage = () => {
+    console.log(values)
+    values.step = 0
+    values.validUser = (true)
+  }
+
   return (
     <Box sx={{ width: '100%' }}>
-      <h3> TIME LEFT IS {(mins > 9) ? mins : `0${mins}`} : {(seconds > 9) ? (seconds !== 60 && seconds) : `0${seconds}`} ! </h3>
-      <div className='stepsLabel'>
-        {QuizQues.map((ques, index) => (
-          < div >
-            <button className={activeStep === index ? `activePage` : 'regularButton'} onClick={() => handleTabs(index)}> {index + 1} </button>
-          </div>
-        ))}
-      </div>
-      {
-        activeStep === QuizQues.length ? (
-          <>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              <Result />
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </>
-        ) : (
-          <>
-            <form>
-              {QuizQues.map((ques, index) => (
-                index === activeStep &&
-                <div className='mainForm' key={index}> {ques.question}
-                  {ques.options.map((queOption) => (
-                    <>
-                      <br /><label>
-                        <input type='radio' name={`${index + 1}`} value={queOption} onChange={(e) => handleChange(e)} defaultChecked={answers?.filter((item: any) => (item?.ans === queOption)).length > 0} />
-                        {queOption} </label></>
-                  ))}
-                </div>
-              ))}
-              <br />
-              <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                {isStepOptional(activeStep) && (
-                  <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }} disabled={activeStep >= QuizQues.length - 1}>
-                    Skip
-                  </Button>
-                )}
-                <MobileStepper
-                  variant="progress" steps={QuizQues.length} position="static" activeStep={activeStep} sx={{ maxWidth: 400, flexGrow: 1 }}
-                  nextButton={
-                    <Button size="small" onClick={handleNext}>
-                      {activeStep === QuizQues.length - 1 ? 'FINISH' : 'NEXT'}
-                      {theme.direction === 'rtl' ? (
-                        <KeyboardArrowLeft />
-                      ) : (
-                        <KeyboardArrowRight />
-                      )}
-                    </Button>
-                  }
-                  backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                      {theme.direction === 'rtl' ? (
-                        <KeyboardArrowRight />
-                      ) : (
-                        <KeyboardArrowLeft />
-                      )}
-                      BACK
-                    </Button>
-                  }
-                />
-              </Box>
-            </form>
-          </>
-        )
-      }
+      <form>
+        {
+          values.validUser ?
+            <>
+              <h3> Welcome {values.name}! </h3>
+              <h3> TIME LEFT IS {(mins > 9) ? mins : `0${mins}`} : {(seconds > 9) ? (seconds !== 60 && seconds) : `0${seconds}`} ! </h3>
+              <div className='stepsLabel'>
+                {QuizQues.map((ques, index) => (
+                  < div >
+                    <button type='button' className={values.step === index ? `activePage` : 'regularButton'} onClick={() => handleTabs(index)}> {index + 1} </button>
+                  </div>
+                ))}
+              </div>
+              {
+                values.step === QuizQues.length ? (
+                  <>
+                    <Typography sx={{ mt: 2, mb: 1 }}>
+                      <Result />
+                    </Typography>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                      <Box sx={{ flex: '1 1 auto' }} />
+                      <Button onClick={handleReset}>Reset</Button>
+                    </Box>
+                  </>
+                ) : (
+                  <>
+                    {QuizQues.map((ques, index) => (
+                      index === values.step &&
+                      <div className='mainForm' key={index}> {ques.question}
+                        {ques.options.map((queOption) => (
+                          <>
+                            <br /><label>
+                              <input type='radio' name={`${index + 1}`} value={queOption} onChange={(e) => handleChoice(e)} defaultChecked={values.answers?.filter((item: any) => (item?.ans === queOption)).length > 0} />
+                              {queOption} </label></>
+                        ))}
+                      </div>
+                    ))}
+                    <br />
+                    <Typography sx={{ mt: 2, mb: 1 }}>Step {values.step + 1}</Typography>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                      <MobileStepper
+                        variant="progress" steps={QuizQues.length} position="static" activeStep={values.step} sx={{ maxWidth: 400, flexGrow: 1 }}
+                        nextButton={
+                          <Button size="small" onClick={handleNext} disabled={values.answers.filter((check: any) => Number(check.que) === Number(values.step + 1)).length === 0}>
+                            {values.step === QuizQues.length - 1 ? 'FINISH' : 'NEXT'}
+                            {theme.direction === 'rtl' ? (
+                              <KeyboardArrowLeft />
+                            ) : (
+                              <KeyboardArrowRight />
+                            )}
+                          </Button>
+                        }
+                        backButton={
+                          <Button size="small" onClick={handleBack} disabled={values.step === 0}>
+                            {theme.direction === 'rtl' ? (
+                              <KeyboardArrowRight />
+                            ) : (
+                              <KeyboardArrowLeft />
+                            )}
+                            BACK
+                          </Button>
+                        }
+                      />
+                    </Box>
+                  </>
+                )
+              }
+            </>
+            :
+            <div>
+              <label> ENTER YOUR USERNAME :
+                <input type='text' name='name' value={values.name} onChange={handleChange} />
+              </label>
+              <button type='button' onClick={() => handlePage()}> NEXT </button>
+            </div>
+        }
+      </form>
     </Box >
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -14,13 +14,37 @@ function Result() {
                 ((item.correctAnswer === quizItem.ans) ? score += 1 : score) : score
         ))
     ))
-
+    console.log(userInputs.map((quizItem) => (
+        QuizQues.filter((item, index) => (
+            ((index + 1) === Number(quizItem.que) &&
+                { item }
+            ))))), "user")
     return (
         <>
             <h2> NO. OF QUESTIONS ATTEMPTED : {userInputs?.length} / {QuizQues.length} </h2>
             <h3> QUES SKIPPED : {QuizQues.length - userInputs?.length} </h3>
-            <h3> YOUR OVERALL RESULT IS : {score} MARKS </h3>
-            <h3> No. OF INCORRECT ARE : {userInputs?.length - score} </h3>
+            <h3> <u> YOUR OVERALL RESULT IS : {score} MARKS </u> </h3>
+            <h3> INCORRECTLY ANSWERED ARE : {userInputs?.length - score} </h3>
+
+            {userInputs.map((quizItem, i) => (
+                QuizQues.filter((notItem, index) => (
+                    ((index + 1) === Number(quizItem?.que)))).map((item) =>
+                        <div className='mainForm'>
+                            <h4>{i + 1} {item?.question} </h4>
+                            {item.options.map((opt) =>
+                                <>
+                                    {(quizItem.ans === item.correctAnswer && opt === item.correctAnswer) ?
+                                        <div className='correctAnswer'> {opt} </div> :
+                                        (opt === quizItem.ans && quizItem.ans !== item.correctAnswer) ?
+                                            <div className='wrongAnswer'> {opt} </div> :
+                                            (opt === item.correctAnswer && quizItem.ans !== item.correctAnswer) ?
+                                                <div className='correctAnswer'> {opt} </div> : <div> {opt} </div>
+                                    }
+                                </>
+                            )}
+                        </div>)
+            ))
+            }
         </>
     )
 }
